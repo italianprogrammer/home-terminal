2c3bcc01603b2b642199b47900049bb9b313a530
Sofia Vicedomini <11537307+blacksoulgem95@users.noreply.github.com>
2020-06-11 17:07:08 +0100

    project init

---
+++ README.md
@@ -0,0 +1,2 @@
+# Home Terminal
+#### A Fallout inspired terminal to handle all your Smart Home devices 
---
+++ src/main/java/pizza/italianprogrammer/hometerminal/Application.java
@@ -0,0 +1,8 @@
+package pizza.italianprogrammer.hometerminal;
+
+public class Application {
+
+    public static void main(String[] args) {
+
+    }
+}
